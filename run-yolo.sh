30f2069a9d27caa6c84a82cfb3286d0c9c9edd38
Rakshith Vasudev <rakshith_vasudev@dellteam.com>
2019-08-08 10:36:07 -0500

    adding runnable script

---
+++ run-yolo.sh
@@ -0,0 +1,34 @@
+# Written by Seth Barberee, Undergraduate Intern
+# Created on 7/10/19
+# Using a pre-built image from the YOLO directory,
+# run YOLO containerized in Docker
+
+
+#! /bin/bash
+
+# Explanation of flags
+# -v /dev/video - let container access webcam on video0
+
+# Using the below flags, connect X11 server to Docker
+# -v /tmp/.X11-unix
+# -e DISPLAY=$DISPLAY
+
+
+# Make sure webcam is there
+if [ ! -d `/dev/video0` ]; then
+	echo "=> Is the webcam plugged in?"
+	exit 1
+fi
+
+# IF the directory is there but not symlink, we gotta delete it
+if [ ! -L `/dev/video0` ]; then
+	echo "=> rm -r /dev/video0 and replug webcam"
+	exit 1
+fi
+
+# Shim up X11 display to docker
+docker run --rm --privileged -v /dev/video0:/dev/video0 \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e DISPLAY=$DISPLAY \
+	--name "yolo-docker" \
+	yolo:centos
